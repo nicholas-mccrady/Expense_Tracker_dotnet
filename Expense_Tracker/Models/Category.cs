d647f40b8ad6112ac2cc914331fbf4559af4906f
nicholas-mccrady <nicholas.mccrady@gmail.com>
2022-12-22 23:05:45 -0600

    added category and transaction models working on migrations

---
+++ Expense_Tracker/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+// calling the component models and creating a namespace 
+namespace Expense_Tracker.Models
+{
+ // creating a primary key as well as our category icons
+    public class Category
+    {
+        // use short cut alt+enter to set up our key in this case the categoryId
+        [Key]
+        //initialize our getters and setters for the categories
+        public int CategoryId { get; set; }
+
+        // creating our sql server datatypes and calling the appropriate namespace with it
+        // Names the titles of each category
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        //each category will have an emoji icon
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; }
+
+        //type of category weather its income or expense
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; }
+
+    }
+}
---
+++ Expense_Tracker/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        //as before create the primary key of the class
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryId needs to be saved here which is a forieng
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+
+        public DateTime date { get; set; } = DateTime.Now;
+    
+    }
+}
